9241cea810ed7e2125c675b543e0523cde701978
Reyes546 <sarahwood@Sarahs-MacBook-Air-2.local>
2024-05-12 19:42:01 -0700

    added broken subcommand

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # usab
 Basic rust async server
+
+Runs async server. Pass command line arguements like so from the home directory:
+```bash
+cargo run -- --help
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use tide::Request;
 use tide::prelude::*;
 use std::env;
+use clap::{ arg, Arg, ArgAction, command, Command, value_parser };
 
 #[derive(Debug, Deserialize)]
 struct ArdReq {
@@ -9,7 +10,29 @@ struct ArdReq {
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let path = env::current_dir()?;
+
+    let matches = command!() // requires `cargo` feature
+        .arg(
+            Arg::new("debug").short('d').action(ArgAction::SetTrue)
+        )
+        .subcommand(
+            Command::new("run")
+                .about("specify which fucntionality you'd like to run")
+                .arg(arg!(-l --list "lists test values")
+                    .action(ArgAction::SetTrue))
+                .arg(arg!(<SETUP>)
+                    .value_parser(value_parser!(u16).range(1..)))
+        )
+        .get_matches();
+
+    let setup: u16 = *matches
+    .get_one::<u16>("SETUP")
+    .expect("'SETUP' is required and parsing will fail if its missing");
+    println!("PORT = {setup}");
+
+    println!("name: {:?}", matches.get_one::<String>("name"));
+
+    let path: std::path::PathBuf = env::current_dir()?;
     println!("The current directory is {}", path.display());
     let mut app = tide::new();
     app.at("/:identifier/rgb").post(set_rgb);
